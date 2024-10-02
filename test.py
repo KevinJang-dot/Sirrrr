b0b5ad0cc2c1aa0a453ac965928d9e9b37e871e1
KevinJang-dot <jangkevinjayd@gmail.com>
2024-10-03 00:12:07 +0800

    papasara ko sir

---
+++ test.py
@@ -0,0 +1,12 @@
+num1 = int(input("num1:"))
+num2 = int(input("num2: "))
+op = input('choose [+-*/]')
+
+if op == '+':
+    print(num1+num2)
+elif op == '-':
+    print(num1-num2)
+elif op == '*':
+    print(num1*num2)
+elif op == '/':
+    print(num1/num2)
